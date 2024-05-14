d5f62fc09d1dfdc322bda95fdc3998b663732c65
aaronvidanalora <120584227+aaronvidanalora@users.noreply.github.com>
2024-05-14 18:33:17 +0000

    tasca 3 historia 2 acabada

--- src/componentes/Cards.jsx
+++ src/componentes/Cards.jsx
@@ -3,16 +3,15 @@ import Carta from "./Card";
 
 const Cards = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
-      {historias.map((historia) => (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4 sm:pl-32 sm:pr-32 ">
+      {historias.map((historia, index) => (
         <Carta
-          key={historia.id}
-          historia={historia}
+          key={index}
+          id={historia.id}
           titulo={historia.titulo}
           fecha={historia.fecha}
           experiencia={historia.experiencia}
-          comentario={historia.comentario}
-          imagen={historia.imagen}
+          imagen={historia.imagen}    
         />
       ))}
     </div>
